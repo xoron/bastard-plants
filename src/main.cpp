e28eec4c666e7286b1888e65056dd1c2c7d0ed0b
raju <rmaher84@gmail.com>
2020-02-03 20:29:14 +0100

    latest changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,49 @@
 #include <Arduino.h>
-#include "./io/valve/valve.h"
-#include "./io/water-sensor/water-sensor.h"
-#include "./io/temperature-humidity-sensor/temperature-humidity-sensor.h"
-#include "./io/stepper-motor/stepper-motor.h"
+// #include "./io/valve/valve.h"
+// #include "./io/water-sensor/water-sensor.h"
+// #include "./io/temperature-humidity-sensor/temperature-humidity-sensor.h"
+// #include "./io/stepper-motor/stepper-motor.h"
+
+#include "components/stepper-motor/stepper-motor.h"
+#include "components/servo-motor/Servo.h"
+
+// const int stepsPerRevolution = 400;  // change this to fit the number of steps per revolution
+// // for your motor
+
+// // initialize the stepper library on pins 8 through 11:
+// Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int potpin = 0;  // analog pin used to connect the potentiometer
+int val;    // variable to read the value from the analog pin 
+
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.print("hello wolrd in setup()");
+  // // set the speed at 60 rpm:
+  // myStepper.setSpeed(25);
+  // // initialize the serial port:
+  // Serial.begin(9600);
+
+  myservo.attach(7);  // attaches the servo on pin 9 to the servo object
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // // step one revolution  in one direction:
+  // Serial.println("clockwise");
+  // Serial.println(stepsPerRevolution);
+  // myStepper.step(stepsPerRevolution);
+  // delay(1000);
 
-  int soilMoistureLevel = getSoilMoistureLevel(); // get soil moisture level
-  int temperatureLevel = getTemperature(); // get temperature
-  int humidityLevel = getHumidityLevel(); // get temperature
+  // // step one revolution in the other direction:
+  // Serial.println("counterclockwise");
+  // Serial.println((-stepsPerRevolution));
+  // myStepper.step((-stepsPerRevolution));
+  // delay(1000);
 
-  openValve();
-  delay(1000);
-
-  Serial.print("hello wolrd");
+  val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023) 
+  val = map(val, 0, 1000, 0, 360);     // scale it to use it with the servo (value between 0 and 180) 
+  myservo.write(val);                  // sets the servo position according to the scaled value 
+  delay(15);
 }
